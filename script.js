bdc47e5d38ce9186e901027a1a661cbd01d6a515
umar0055 <umarsohail648@gmail.com>
2024-12-13 19:57:44 +0500

    assigment no 7

---
+++ script.js
@@ -0,0 +1,130 @@
+//assigmnet no 1
+// var userinput=prompt("Enter 1 to turn on light and 0 to turn off the light ");
+// if(userinput == 1 ){
+//   console.log("the light is on")
+// }else if(userinput == 0)
+//   {
+//     console.log("the light is off")
+// }else{
+//   console.log("invilad option")
+// }
+
+
+
+// assigment no 2
+// var userinput=prompt("Enter colour and colour must be red yellow green ");
+// var stop = "red";
+// var slow = "yellow";
+// var start = "green";
+// if(userinput == stop){
+//   console.log( " stop")
+// }else if(userinput == slow){
+//   console.log("slow down")
+// }else if(userinput == start){
+//   console.log("go")
+// }else{
+//   console.log("invilade option")
+// }
+
+// assigment no 3
+
+// var userinput=prompt("Enter temputare ");
+// if(userinput <= 0 ){
+//   console.log("freezing")
+// }else if(userinput > 0  && userinput <= 15 ) {
+//     console.log("cold")
+// }else if(userinput > 16 && userinput<= 30 ) {
+//     console.log("warm")
+// }else{
+//   console.log("hot")
+// }
+
+// assigment no 4
+
+
+// assigment no 5
+// var userinput =prompt("Is the door open? (yes or no)");
+// var yes = "yes";
+// var no = "no";
+// if(userinput == yes || userinput == no){
+// userinput =prompt("Is motion detected? (yes or no)")
+//  if(userinput == yes){
+//   console.log("Alarm Triggered");
+//   }
+//   if(userinput == no){
+//     console.log("all safe")
+//   }
+// }
+
+// assigment no 6 
+
+
+// assigment no 7
+// var userpas=prompt("Enter password ");
+// var length =12345678;
+// var length2 = 123456;
+// var length3 = 1234567;
+// var length4 =12345;
+// if(userpas >= length){
+//   console.log( "strong password")
+// }else if(userpas == length2 || userpas == length3 ){
+//   console.log("Moderate Password")
+// }else if(userpas <= length4 ) {
+//   console.log("Weak Password")
+// }
+
+//assigemnt no 8
+// var user=prompt("enter 1 for head and 0 for tail");
+// var head =1;
+// var tail=0;
+// var num= 4
+// if(user == head){
+//   if(num % 2===0){
+//     console.log("you win")
+//   }
+// }
+// else{
+//   console.log("you lose")
+// }
+
+// assigment no 9 
+// var userinput=prompt("Enter your weather ");
+// var sunny = "sunny";
+// var rainy = "rainy";
+// var cold = "cold";
+// if(userinput == sunny){
+//   console.log( " Wear sunglasses")
+// }else if(userinput ==rainy){
+//   console.log("Take an umbrella")
+// }else if(userinput == cold){
+//   console.log("Wear a jacket")
+// }else{
+//   console.log("invilade option")
+// }
+
+
+// assigment no 10
+// var uservalue= prompt(" solve this statment : 2*23+54/123%76-12");
+// if(uservalue == 34.4){
+//   console.log("wow u are genius")
+// }else{
+//     console.log("try again until u succeed")
+// }
+
+//assigment no 11
+
+// var userage =prompt("Enter your age");
+// if(userage>=0 && userage<=17){
+//    console.log("sorry u are not eligible for vote")
+// }else{
+//      console.log(" u are eligible for vote")
+// }
+
+//assigment no 12
+// var firstnum= prompt("Enter number");
+// var secondnum=prompt("Enter number");
+// if(firstnum >= secondnum){
+//    console.log(`first user win his num was ${firstnum}`)
+// }else{
+//    console.log(`second user win his num was ${secondnum}`)
+// }
